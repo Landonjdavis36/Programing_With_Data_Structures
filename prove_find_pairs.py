89cb0d6b7637584c136dfc143f31a01d3f7a83c4
Landon Davis <70351965+Landonjdavis36@users.noreply.github.com>
2023-09-07 13:29:46 -0600

    Add files via upload

---
+++ prove_find_pairs.py
@@ -0,0 +1,32 @@
+"""
+CSE212 
+(c) BYU-Idaho
+05-Prove - Problem 2
+
+It is a violation of BYU-Idaho Honor Code to post or share this code with others or 
+to post it online.  Storage into a personal and private repository (e.g. private
+GitHub repository, unshared Google Drive folder) is acceptable.
+"""
+
+def find_pairs(words):
+    pairs = set()
+    for (a, b) in words:
+        pairs.add((a, b))
+    if (a, b) == (a, a) or (a, b) == (b, b):
+        pairs.remove((a, b))
+    if (b, a) in pairs:
+        print((a, b), '&', (b, a))
+   
+
+find_pairs(["am","at","ma","if","fi"])      # ma & am, fi & if
+print("=============")
+find_pairs(["ab", "bc", "cd", "de", "ba"])  # ba & ab
+print("=============")
+find_pairs(["ab","ba","ac","ad","da","ca"]) # ba & ab, da & ad, ca & ac
+print("=============")
+find_pairs(["ab", "ac"])                    # None
+print("=============")
+find_pairs(["ab", "aa", "ba"])              # ba & ab
+print("=============")
+find_pairs(["23","84","49","13","32","46","91","99","94","31","57","14"])
+                                            # 32 & 23, 94 & 49, 31 & 13
